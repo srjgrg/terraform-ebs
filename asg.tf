af464b6cee1e7be686079340f4949acf24540a14
srjgrg <srjlamichane@gmail.com>
2020-03-09 20:59:11 -0400

    a

--- asg.tf
+++ asg.tf
@@ -1,7 +1,7 @@
 resource "aws_autoscaling_group" "default" {
   name                      = "terraform-ebs"
-  max_size                  = 1
-  min_size                  = 1
+  max_size                  = 3
+  min_size                  = 2
   health_check_grace_period = 300
   health_check_type         = "ELB"
   desired_capacity          = 1
