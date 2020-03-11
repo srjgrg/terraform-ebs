4575d6c3d43ebff2c77a0f6d37423c60db0148a4
srjgrg <srjlamichane@gmail.com>
2020-03-10 20:10:25 -0400

    Update user-data.sh

--- script/user-data.sh
+++ script/user-data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pip install aws
+
 vol=vol-05bc5599c17cfa82b
 mount_dir=/suraj
 device=/dev/xvdf/
@@ -16,6 +18,9 @@ instance_id=`curl http://169.254.169.254/latest/meta-data/instance-id`
 echo "this command is to attach EBS to EC2"
 
 aws ec2 attach-volume --volume-id $vol --instance-id $instance_id --device /dev/sdf
+
 sleep 60 
-echo "mount filesystem to folder" 
+
+echo "mount filesystem to folder"
+
 mount $device $mount_dir
